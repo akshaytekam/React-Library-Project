9e3cacce0b352a3c660ca427d070d17c365e5032
Akshay Tekam <akshaytekam@atlassian.com>
2022-03-06 00:34:18 +0530

    Done

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const express = require("express");
 const app = express();
 const expresslayouts = require("express-ejs-layouts") 
 
+const methodOverride = require('method-override')
 const indexRouter = require("./routes/index")
 const authorRouter = require("./routes/authors")
 const bodyParser = require('body-parser')
+const bookRouter = require('./routes/books')
 
 const mongoose = require("mongoose");  //import mongoose db
 mongoose.connect(process.env.DATABASE_URL, {    //connected to DB on web
@@ -25,9 +27,11 @@ app.set("layout", "layouts/layout") //set up layouts folder
 app.use(expresslayouts)           // use layouts
 app.use(express.static("public")) //take all my static files from public, like images, css,..
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(methodOverride('_method'))
 
-app.use("/", indexRouter)
+//app.use("/", indexRouter)
 app.use("/authors", authorRouter)
+app.use('/books', bookRouter)
 
 
 app.listen(process.env.PORT || 3000) //we pull port from env variable, and server gonna tell use which port to listen to
